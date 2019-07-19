15bdeab0700e55e966726991720204f3323243cb
Harzz <basuharish97@gmail.com>
2019-07-19 21:46:39 +0530

    created dao for contacts

--- app/src/main/java/io/github/harzz/kontacts/repository/dao/ContactsDao.kt
+++ app/src/main/java/io/github/harzz/kontacts/repository/dao/ContactsDao.kt
@@ -1,2 +1,33 @@
 package io.github.harzz.kontacts.repository.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import io.github.harzz.kontacts.repository.entity.Contacts
+
+@Dao
+interface ContactsDao {
+
+    //insert a new contact
+    @Insert
+    fun insert(contacts: Contacts)
+
+    //delete all contacts
+    @Query("DELETE FROM contacts_table")
+    fun deleteAllNotes()
+
+    //return only users who belong to current_user
+    @Query("SELECT user_name,user_phone_number from contacts_table where owner = :owner")
+    fun getContacts(owner:String) : LiveData<List<Contacts>>
+
+    //delete a particular contact
+    @Query("Delete from contacts_table where id = :_id")
+    fun deleteSingleContact(_id : Int)
+
+    //update the current user
+    @Update
+    fun updateSingleUser( contacts: Contacts)
+
+}
