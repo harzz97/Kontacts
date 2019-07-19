4fcc1234b3b4d99a907889c852a2fb5ec968b3aa
Harzz <basuharish97@gmail.com>
2019-07-19 21:46:07 +0530

    repository with wrapper methods

--- app/src/main/java/io/github/harzz/kontacts/repository/ContactsRepository.kt
+++ app/src/main/java/io/github/harzz/kontacts/repository/ContactsRepository.kt
@@ -1,4 +1,60 @@
 package io.github.harzz.kontacts.repository
 
-class ContactsRepository {
+import android.app.Application
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+import io.github.harzz.kontacts.repository.dao.ContactsDao
+import io.github.harzz.kontacts.repository.database.ContactsDatabase
+import io.github.harzz.kontacts.repository.entity.Contacts
+
+class ContactsRepository(application: Application) {
+
+    private var contactsDao : ContactsDao
+
+//    private var contactsList : LiveData<List<Contacts>>
+
+    init {
+        val database : ContactsDatabase = ContactsDatabase.getInstance(application.applicationContext)
+        contactsDao = database.contactsDao()
+//        contactsList = contactsDao.getContacts("Harzz")
+
+    }
+
+    //function calls another async fun to perform the job
+    fun insertContact(contacts: Contacts){
+        InsertContactsAsyncTask(contactsDao).execute(contacts)
+    }
+
+    fun deleteContact(contacts: Contacts){
+//        DeleteContactsAsyncTask(contactsDao).execute(contacts.id)
+    }
+
+    fun updateContact(contacts: Contacts){
+        UpdateContactsAsyncTask(contactsDao).execute(contacts)
+    }
+
+    fun getAllContacts() : LiveData<List<Contacts>>{
+        return contactsDao.getContacts("Harzz");
+    }
+
+    private class InsertContactsAsyncTask(val contactsDao: ContactsDao) : AsyncTask<Contacts,Unit,Unit>(){
+        override fun doInBackground(vararg p0: Contacts?) {
+            contactsDao.insert(p0[0]!!)
+        }
+    }
+
+    private class DeleteContactsAsyncTask(val contactsDao: ContactsDao) : AsyncTask<Contacts,Unit,Unit>(){
+
+        override fun doInBackground(vararg p0: Contacts?) {
+            //TODO:decide whether to pass id or contact object
+//            contactsDao.deleteSingleContact(p0[0]!!)
+        }
+    }
+
+    private class UpdateContactsAsyncTask(val contactsDao: ContactsDao) : AsyncTask<Contacts,Unit,Unit>(){
+        override fun doInBackground(vararg p0: Contacts?) {
+            contactsDao.updateSingleUser(p0[0]!!)
+        }
+    }
+
 }
