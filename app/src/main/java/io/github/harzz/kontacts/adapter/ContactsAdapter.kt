3864d4ae11cf42f73d86fa8e30deb5779dddb13a
Harzz <basuharish97@gmail.com>
2019-07-19 21:45:41 +0530

    initial contacts adapter

--- app/src/main/java/io/github/harzz/kontacts/adapter/ContactsAdapter.kt
+++ app/src/main/java/io/github/harzz/kontacts/adapter/ContactsAdapter.kt
@@ -1,4 +1,40 @@
 package io.github.harzz.kontacts.adapter
 
-class ContactsAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.github.harzz.kontacts.R
+import io.github.harzz.kontacts.repository.entity.Contacts
+import kotlinx.android.synthetic.main.content_row_recycler_view.view.*
+
+class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>() {
+
+    private var allContacts : List<Contacts> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.content_row_recycler_view,parent,false)
+        return ContactsViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return allContacts.size
+    }
+
+    override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
+        val currentContact = allContacts.get(position)
+        holder.userName.text = currentContact.user_name
+        holder.userPhoneNumber.text = currentContact.user_phone_number
+    }
+
+    fun setNotes(contacts : List<Contacts>) {
+        allContacts = contacts
+        notifyDataSetChanged()
+    }
+
+    inner class ContactsViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        var userName = itemView.txt_user_name
+        var userPhoneNumber = itemView.txt_phone_number
+    }
 }
