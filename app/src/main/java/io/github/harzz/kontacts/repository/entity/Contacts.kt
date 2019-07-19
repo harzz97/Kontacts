af9679f15eed263c522f0747075aba000a51443f
Harzz <basuharish97@gmail.com>
2019-07-19 21:47:38 +0530

    created contact entities

--- app/src/main/java/io/github/harzz/kontacts/repository/entity/Contacts.kt
+++ app/src/main/java/io/github/harzz/kontacts/repository/entity/Contacts.kt
@@ -1,2 +1,14 @@
 package io.github.harzz.kontacts.repository.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts_table")
+data class Contacts (
+    var owner : String,
+    var user_name : String,
+    var user_phone_number: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id:Int = 0
+}
