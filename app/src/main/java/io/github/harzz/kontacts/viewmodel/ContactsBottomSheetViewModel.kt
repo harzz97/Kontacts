c22cab0ac2bfa2a2a119cc8a8e934014d5d1cf5d
Harzz <basuharish97@gmail.com>
2019-07-20 00:41:40 +0530

    added functions to edit delete and save

--- app/src/main/java/io/github/harzz/kontacts/viewmodel/ContactsBottomSheetViewModel.kt
+++ app/src/main/java/io/github/harzz/kontacts/viewmodel/ContactsBottomSheetViewModel.kt
@@ -1,7 +1,19 @@
-package io.github.harzz.kontacts
+package io.github.harzz.kontacts.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel;
+import io.github.harzz.kontacts.repository.ContactsRepository
+import io.github.harzz.kontacts.repository.entity.Contacts
 
-class ContactsBottomSheetViewModel : ViewModel() {
+class ContactsBottomSheetViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var contactsRepository = ContactsRepository(application)
+
+    fun createContact(contact: Contacts) = contactsRepository.insertContact(contact)
+
+    fun updateContact(contact: Contacts) = contactsRepository.updateContact(contact)
+
+    fun deleteContact(contact: Contacts) = contactsRepository.deleteContact(contact)
 
 }
