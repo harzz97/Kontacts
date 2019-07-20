8726cec53ef5a608232fd01add222aeca808272b
Harzz <basuharish97@gmail.com>
2019-07-20 23:15:46 +0530

    added new login activity

---
+++ app/src/main/java/io/github/harzz/kontacts/LoginActivity.kt
@@ -0,0 +1,4 @@
+package io.github.harzz.kontacts
+
+class LoginActivity {
+}
---
+++ app/src/main/java/io/github/harzz/kontacts/repository/dao/UsersDao.kt
@@ -0,0 +1,15 @@
+package io.github.harzz.kontacts.repository.dao
+
+import androidx.room.Dao
+
+@Dao
+interface User{
+
+    //insert a new user
+
+
+    //update user
+
+
+    //delete user
+}
