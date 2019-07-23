25c3d77602e0b5f67e510bcbc5e4ca3d1d6eca0d
Harzz <basuharish97@gmail.com>
2019-07-23 16:36:15 +0530

    rewrote methods

--- app/src/main/java/io/github/harzz/kontacts/viewmodel/LoginViewModel.kt
+++ app/src/main/java/io/github/harzz/kontacts/viewmodel/LoginViewModel.kt
@@ -2,12 +2,21 @@ package io.github.harzz.kontacts.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import io.github.harzz.kontacts.repository.UsersRepository
 import io.github.harzz.kontacts.repository.entity.Users
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private var usersRepository = UsersRepository(application)
+    fun createNewUser(users: Users)  {
+        usersRepository.insertNewUser(users)
+    }
 
-    fun insert(users: Users) = usersRepository.insertNewUser(users)
+    fun getUserDetails(currentUser : Users) : LiveData<Users>?{
+        return usersRepository.getSingleUser(currentUser.email)
+    }
+    fun getAllUsers() : LiveData<List<String>>?{
+        return usersRepository.getAllUser()
+    }
 }
