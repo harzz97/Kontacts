42e009fdf0d5185bc6f5373e4dac9cc98a461f2b
Harzz <basuharish97@gmail.com>
2019-07-23 16:33:55 +0530

    changed primary key

--- app/src/main/java/io/github/harzz/kontacts/repository/entity/Users.kt
+++ app/src/main/java/io/github/harzz/kontacts/repository/entity/Users.kt
@@ -6,9 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "users_table")
 data class Users(
     var userName : String,
-    var userPassword : String
-){
-
-    @PrimaryKey(autoGenerate = true)
-    var id : Int = 0
-}
+    var userPassword : String,
+    @PrimaryKey
+    var email : String
+)
