c050560b9733196e8e92b8db9fa9b2380f2d3261
Harzz <basuharish97@gmail.com>
2019-07-23 16:31:46 +0530

    conflict stratergy and new methods

--- app/src/main/java/io/github/harzz/kontacts/repository/dao/UsersDao.kt
+++ app/src/main/java/io/github/harzz/kontacts/repository/dao/UsersDao.kt
@@ -1,7 +1,9 @@
 package io.github.harzz.kontacts.repository.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import io.github.harzz.kontacts.repository.entity.Users
 
@@ -9,14 +11,15 @@ import io.github.harzz.kontacts.repository.entity.Users
 interface UsersDao{
 
     //insert a new user
-    @Insert
-    fun insert(users: Users)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(users: Users) : Long
 
     //validate user account
-    @Query("Select * from users_table where userName = :userName ")
-    fun getUserDetails(userName : String) : Users
+    @Query("Select * from users_table where email = :email ")
+    fun getUserDetails( email : String) : LiveData<Users>
 
+    @Query("Select email from users_table")
+    fun getAllUsers() : LiveData<List<String>>
 
     //delete user
-
 }
