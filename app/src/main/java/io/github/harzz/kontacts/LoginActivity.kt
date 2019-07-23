9ee57fd0e43fb77bb2beb55af6cbb18b40e3cebf
Harzz <basuharish97@gmail.com>
2019-07-23 16:27:37 +0530

    now redirect logged in users to home screen and added validation msgs

--- app/src/main/java/io/github/harzz/kontacts/LoginActivity.kt
+++ app/src/main/java/io/github/harzz/kontacts/LoginActivity.kt
@@ -2,46 +2,131 @@ package io.github.harzz.kontacts
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.os.Handler
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import io.github.harzz.kontacts.repository.entity.Users
+import io.github.harzz.kontacts.utils.SharedPrefManager
+import io.github.harzz.kontacts.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var alertDialog: AlertDialog
+    var isUserSuccess : Boolean = false
+    private  lateinit var allUsers : LiveData<List<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
-
-
-    }
-
-    fun onClickLogin(view : View){
-        if(isValidLogin()){
-            Toast.makeText(this, "Valid login!", Toast.LENGTH_SHORT).show()
-
-            /*val bundle = Bundle()
-            bundle.putString("OwnerName", etEmail?.text.toString())
-            val frag = AddContactBottomSheetFragment()
-            frag.arguments = bundle*/
-
-            val homePageIntent = Intent(this, MainActivity::class.java)
-            startActivity(homePageIntent)
-
+        //if user not logged in
+        if(SharedPrefManager(this).isLoggedIn){
+            moveToHomeActivity()
         }else{
-            //show error msg via toast or textbox error msg
+            loginViewModel  = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+            Thread(Runnable {
+                allUsers = loginViewModel.getAllUsers()!!
+            }).start()
         }
     }
 
-    private fun isValidLogin() : Boolean{
+    //when signup text is pressed
+    fun displaySignUpDialog(view: View){
+        val openDialog = AlertDialog.Builder(this)
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_signup,null,false)
+        openDialog.setView(dialogView)
+        alertDialog = openDialog.create()
+        alertDialog.show()
+    }
+
+    //when signup button is pressed
+    fun onClickSignup(view:View){
+        if(isValidSignup()){
+               createNewUser()
+        }else{
+            Toast.makeText(this,"Pls enter valid details",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //when login button is pressed
+    fun onClickLogin(view : View){
+        if(isValidLoginDetails()){
+            val currUserEmail = editText_login_user_email.text.toString()
+            val currentUserPassword = editText_login_user_password.text.toString().sha256()
+            val user = Users("",currentUserPassword,currUserEmail)
+
+            loginViewModel.getUserDetails(user)?.observe(this,
+                Observer {
+                if(it != null){
+
+                    if(it.email == currUserEmail){
+                        if(it.userPassword == currentUserPassword){
+                            storeSharedPref(currUserEmail)
+                            moveToHomeActivity()
+                        }else{
+                            editText_login_user_password.error = "Enter valid password"
+                        }
+                    }
+                }else{
+                    Toast.makeText(this,"Pls Sign up First",Toast.LENGTH_LONG).show()
+                }
+            })
+        }else{
+            //show error msg via toast or textbox error msg
+            Toast.makeText(this,"Invalid Login Details",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isValidSignup():Boolean{
+        val email = alertDialog.findViewById<EditText>(R.id.editText_email)!!
+        val password = alertDialog.findViewById<EditText>(R.id.editText_password)!!
+        val rePassword = alertDialog.findViewById<EditText>(R.id.editText_rePassword)!!
+
         //verify email
-        if(!isValidEmail(editText_login_user_email.toString()))  return false
+        if(!isValidEmail(email.text.toString())){
+            email.error = "Pls enter valid email"
+            return false
+        }
         //verify the password
-        if(!isValidPassword(editText_login_user_password.toString())) return false
+        if(!isValidPassword(password.text.toString())){
+            password.error = "Pls enter valid password"
+            return false
+        }
+
+        if(!isValidPassword(rePassword.text.toString())) {
+            rePassword.error = "Pls enter valid password"
+            return false
+        }
+
+        if(!password.text.toString().equals(rePassword.text.toString()))  {
+            rePassword.setText("")
+            Toast.makeText(this,"Passwords don't match",Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        return true
+    }
+
+    private fun isValidLoginDetails() : Boolean{
+        //verify email
+        if(!isValidEmail(editText_login_user_email.text.toString())){
+            editText_login_user_email.error = "Pls enter valid email"
+            return false
+        }
+        //verify the password
+        if(!isValidPassword(editText_login_user_password.text.toString())){
+            Toast.makeText(this,"Pls enter valid password",Toast.LENGTH_LONG).show()
+            return false
+        }
 
         return true
 
@@ -51,7 +136,7 @@ class LoginActivity : AppCompatActivity() {
         return if (target == null) {
             false
         } else {
-            !android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches()
+            android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches()
         }
     }
 
@@ -64,8 +149,47 @@ class LoginActivity : AppCompatActivity() {
             if (password.matches(Regex(".*[A-Z]*."))) strength++
             if (password.matches(Regex(".*\\d*.")))  strength++
             if(password.length >= 6 ) strength++
-            Log.v("LA",strength.toString())
             return strength >= 4
         }
+    }
+
+
+    private fun createNewUser() {
+        val email = alertDialog.findViewById<EditText>(R.id.editText_email)
+        val password = alertDialog.findViewById<EditText>(R.id.editText_password)
+        val userName = alertDialog.findViewById<EditText>(R.id.editText_userName)
+        val user = Users(userName?.text.toString(),password?.text.toString().sha256(),email?.text.toString())
+
+        allUsers.observe(this, Observer<List<String>> {
+            if(it.contains(email!!.text.toString())){
+                if(!isUserSuccess)
+                    Toast.makeText(this,"User Account already exists Pls Login",Toast.LENGTH_LONG).show()
+            }else{
+                isUserSuccess = true
+                loginViewModel.createNewUser(user)
+                alertDialog.dismiss()
+                storeSharedPref(alertDialog.findViewById<EditText>(R.id.editText_email).toString())
+                moveToHomeActivity()
+            }
+        })
+
+    }
+
+    //an extension program to generate sha256 for strings
+    private fun String.sha256():String{
+        val userPasswordBytes = this.toByteArray()
+        MessageDigest.getInstance("SHA-256")
+        return userPasswordBytes.fold("",{str,it->str+"%02x".format(it)})
+    }
+    
+    private fun moveToHomeActivity(){
+        val homePageIntent = Intent(this, MainActivity::class.java)
+        startActivity(homePageIntent)
+        finish()
+    }
+
+    private fun storeSharedPref(userEmail : String){
+        SharedPrefManager(this).isLoggedIn = true
+        SharedPrefManager(this).currentUserInfo = userEmail
     }
 }
